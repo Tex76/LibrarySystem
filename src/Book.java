caaebeab5241a3bd32d2b3e4d50bd3e873fe04b2
maha alzoubah <m202002565@hotmail.com>
2023-12-15 16:41:27 +0300

    updated message

--- src/Book.java
+++ src/Book.java
@@ -28,7 +28,7 @@ public class Book
          this.isbn = isbn;
       else
       {
-         System.out.println("ISBN not 13 light");
+         System.out.println("ISBN must be 13 digit");
          return;
       }
       this.title = title;
