dece49b53bb7291eceaf690d8a555447809d5f5e
Tex <techcenter2001@gmail.com>
2023-12-30 15:04:20 +0300

    add test cases + changes the code of testing

--- src/LibrarySystemTest.java
+++ src/LibrarySystemTest.java
@@ -70,24 +70,53 @@ class LibrarySystemTest {
 
     @Test
     void issueBookDoseNotExists(){
+        // define members and books
+        LibMember member = new  LibMember("Carter","William",'M',10176535,"34567281");
+        Book book =new Book("Forest","Tobin","Shaw","Oscar",1994,"1234100000003",1004);
+        Book book1 =new Book("Animal","Winston","Oberon","Sawyer",1995,"1234100000004",1005);
+        Book book2 =new Book("Trust","Oberon","Kinsley","Lily",1996,"1234100000005",1006);
+        // first we add member to library
         library.addMember(member);
-        assertFalse(library.issueBook(book.getAccessionNum(), member.getCprNum()));
+
+        // add two books
+        library.addBook(book);
+        library.addBook(book1);
+
+        // third book is not in the library so it must return false when I tru to issued to memeber
+        assertFalse(library.issueBook(book2.getAccessionNum(), member.getCprNum()));
     }
 
     @Test
     void issuedBookToMemberDoseNotExists(){
+        LibMember member = new  LibMember("Carter","William",'M',10176535,"34567281");
+        LibMember member1 = new  LibMember("Sara","Ali",'M',23476535,"34567281");
+        Book book =new Book("Forest","Tobin","Shaw","Oscar",1994,"1234100000003",1004);
+
+        library.addMember(member);
         library.addBook(book);
-        assertFalse(library.issueBook(book.getAccessionNum(), member.getCprNum()));
+
+        assertFalse(library.issueBook(book.getAccessionNum(), member1.getCprNum()));
     }
 
 
     @Test
     void issuedAnIssuedBook(){
+        // data that will be insert into the library and it's exsist in it
         LibMember member1 = new LibMember("Olive","Henry",'F',10176534,"34567282");
+        LibMember member = new  LibMember("Carter","William",'M',10176535,"34567281");
+        Book book = new Book("Islands","Ackerley","Acton","Adamaris",1991,"1234100000000",1002);
+
+        // add book to library
         library.addBook(book);
+
+        // add two members to library
         library.addMember(member);
         library.addMember(member1);
+
+        // issued the book to first member
         library.issueBook(book.getAccessionNum(),member.getCprNum());
+
+        // try to issue the same book to another member and test
         assertFalse(library.issueBook(book.getAccessionNum(),member1.getCprNum()));
     }
 
@@ -134,22 +163,64 @@ class LibrarySystemTest {
     }
 
 
+    @Test
+    void issuedBookWithoutBookAndMemberExistence(){
+
+        LibMember member = new  LibMember("Carter","William",'M',10176535,"34567281");
+        LibMember member1 = new  LibMember("Sara","Ali",'M',22396535,"34567281");
+
+
+        Book book =new Book("Forest","Tobin","Shaw","Oscar",1994,"1234100000003",1004);
+        Book book1 =new Book("Story1","Tobin","Shaw","Oscar",1994,"1234100000003",1009);
+        library.addMember(member);
+        library.addBook(book);
+
+        assertFalse(library.issueBook(book1.getAccessionNum(),member1.getCprNum()));
+    }
 
     @Test
     void isBookIssuedExistsIssuedBook() {
+
+        Book book = new Book("Wonders", "Bianca", "Quincy", "Ezekiel", 2000, "1234100000009", 1010);
+        LibMember member =new LibMember("Carter","William",'M',10176535,"34567281");
+
+
+        // book added to library
         library.addBook(book);
         library.addMember(member);
+
+        //issued book to member
         library.issueBook(book.getAccessionNum(),member.getCprNum());
         assertTrue(library.isBookIssued(book.getAccessionNum()));
     }
-    @Test
-    void isBookIssuedBookDoseNotExists(){
-        assertFalse(library.isBookIssued(book.getAccessionNum()));
-    }
+
 
     @Test
     void isBookIssuedBookNotIssuedTest(){
+        // add all books need be issued
+        Book book =new Book("Forest","Tobin","Shaw","Oscar",1994,"1234100000003",1004);
+        Book book1 =new Book("Trust","Oberon","Kinsley","Lily",1996,"1234100000005",1006);
+        LibMember member =new LibMember("Carter","William",'M',10176535,"34567281");
+
+
+        // add books to library and issue only one book
         library.addBook(book);
+        library.addBook(book1);
+        library.issueBook(book.getAccessionNum(), member.getCprNum());
+
+
+
+        // check if second book dose not issued
+        assertFalse(library.isBookIssued(book1.getAccessionNum()));
+    }
+
+    @Test
+    void isBookIssuedBookDoseNotExists(){
+        // define book but not added to library
+            Book book =new Book("Story1","Tobin","Shaw","Oscar",1994,"1234100000003",1009);
+
         assertFalse(library.isBookIssued(book.getAccessionNum()));
     }
+
+
 }
